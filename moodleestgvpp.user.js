c98ceee3c4331394af53142f304827f6241c2792
dicamarques14 <dicamarques14@users.noreply.github.com>
2017-01-12 15:08:00 +0000

    Update and rename moodleestgvpp.js to moodleestgvpp.user.js

--- moodleestgvpp.user.js
+++ moodleestgvpp.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Moodle: muda PDF height e acrescenta link
 // @namespace    https://github.com/dicamarques14/MoodleESTGVPlusPlus
-// @version      2
+// @version      3
 // @description  Muda a altura do iframe dos pdf no moodle e acrescenta link para o pdf no topo da pagina
 // @author       Dg
 // @match        https://moodle.estgv.ipv.pt/*/mod/resource/*
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/dicamarques14/MoodleESTGVPlusPlus/master/moodleestgvpp.meta.js
-// @downloadURL  https://raw.githubusercontent.com/dicamarques14/MoodleESTGVPlusPlus/master/moodleestgvpp.js
-// @updateVersion   2
+// @downloadURL  https://raw.githubusercontent.com/dicamarques14/MoodleESTGVPlusPlus/master/moodleestgvpp.user.js
+// @updateVersion   3
 // ==/UserScript==
 
 var LinkPDF;
