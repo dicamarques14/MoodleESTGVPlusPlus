a68c4c48ca8f4177fbc11654f0d3b72356c2532f
dicamarques14 <dicamarques14@users.noreply.github.com>
2017-01-12 15:01:22 +0000

    Create moodleestgvpp.meta.js

---
+++ moodleestgvpp.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Moodle: muda PDF height e acrescenta link
+// @namespace    https://github.com/dicamarques14/MoodleESTGVPlusPlus
+// @version      2
+// @description  Muda a altura do iframe dos pdf no moodle e acrescenta link para o pdf no topo da pagina
+// @author       Dg
+// @match        https://moodle.estgv.ipv.pt/*/mod/resource/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/dicamarques14/MoodleESTGVPlusPlus/master/moodleestgvpp.meta.js
+// @downloadURL  https://raw.githubusercontent.com/dicamarques14/MoodleESTGVPlusPlus/master/moodleestgvpp.js
+// @updateVersion   2
+// ==/UserScript==
